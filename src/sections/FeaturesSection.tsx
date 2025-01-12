dcd33ec29594dc4bb4b2fb3a6665ca2f718bfb14
ZER-DAY <bahaa.ali.m.alhabil@gmail.com>
2025-01-12 04:14:38 +0300

    bahaa new

--- src/sections/FeaturesSection.tsx
+++ src/sections/FeaturesSection.tsx
@@ -95,7 +95,7 @@ const FeaturesSection = () => {
             <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded-lg hover:bg-blue-600 transition-colors duration-300">
               قدم طلب الانضمام
             </button>
-            <button className="bg-green-500 text-white font-bold py-2 px-4 rounded-lg hover:bg-green-600 transition-colors duration-300">
+            <button className="bg-red-500 text-white font-bold py-2 px-4 rounded-lg hover:bg-green-600 transition-colors duration-300">
               <Link href="/tiktokstep" className="text-white no-underline">
                 قدم طلب الانضمام
               </Link>

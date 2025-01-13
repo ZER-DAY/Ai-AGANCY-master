b93bf0a1476132631311325a9f1c9ec798307e02
ZER-DAY <bahaa.ali.m.alhabil@gmail.com>
2025-01-14 02:22:38 +0300

    bahaa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,14 @@ import { Hero } from "@/sections/Hero";
 import Work from "@/sections/work";
 import { Footer } from "@/sections/Footer";
 import Benefits from "@/sections/benefits";
-
+import About from "@/components/About";
 export default function Home() {
   return (
     <>
       <Hero />
       <Work />
       <Benefits />
+      <About />
     </>
   );
 }
